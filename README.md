e020abdaf23bed6fe588af8a93b05648fb71ead1
tobinbradley <tobin.bradley@gmail.com>
2016-04-04 16:47:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# linter-writing
+
+A package for [Atom](http://atom.io) Linter that performs subjective linting on Markdown documents. This is set up exactly how I like it, but my inner linter/masochist probably isn't tuned the same as yours; use it as a starting point rather than a final solution.
+
+The plugin uses [remark](https://github.com/gnab/remark) and [retext](https://github.com/wooorm/retext), along with [remark-lint](https://github.com/wooorm/remark-lint) (markdown linting), [retext-english](https://github.com/wooorm/retext-english), [retext-equality](https://github.com/wooorm/retext-equality), [retext-profanities](https://github.com/wooorm/retext-profanities), [retext-readability](https://github.com/wooorm/retext-readability), and [retext-simplify](https://github.com/wooorm/retext-simplify), along with some custom filtering for particular words and acronyms.
+
+Credit goes to [linter-markdown](https://github.com/AtomLinter/linter-markdown) for code ideas and [retext-mapbox-standard](https://github.com/mapbox/retext-mapbox-standard) for the inspiration and some industry acronyms. 
---
+++ data/index.js
@@ -0,0 +1,86 @@
+module.exports = {
+    simplifyConfig: {
+        ignore: [
+            'address', // geocoder
+            'request', // technical
+            'interface', // technical
+            'render', // technical
+            'forward', // technical
+            'maximum', // technical
+            'minimum', // technical
+            'type', // technical
+            'initial', // technical
+            'selection', // technical
+            'contains', // technical
+            'implement', // technical
+            'parameters', // technical
+            'function', // technical
+            'option', // technical
+            'effect', // technical
+            'submit', // technical
+            'additional', // sales
+            'might', // may does not have identical connotation
+            'multiple', // many is not equivalent
+            'equivalent', // equal does not have identical connotation
+            'combined', // no good alternative
+            'provide', // i don't think this is really that complicated a word
+            'delete', // this is what the UI says
+            'it is', // no good alternative
+            'there are', // no good alternative
+            'require' // technical
+        ]
+    },
+    equalityConfig: {
+        ignore: [
+            'disabled', // technical
+            'masterpiece', // not offensive enough
+            'host' // technical
+        ]
+    },
+    lintConfig: {        
+		"maximum-line-length": false			
+    },
+	readabilityConfig: {
+		'age': 16
+	}, 
+	profanitiesConfig: {
+		ignore: [
+			'killing'
+		]
+	},
+	myrulesConfig: {
+		acronyms:  [
+			'GeoJSON',
+			'PNG',
+			'SVG',
+			'JPEG',
+			'JPG',
+			'MBTiles',
+			'PDF',
+			'API',
+			'GL',
+			'HTTP',
+			'HTTPS',
+			'TileJSON',
+			'OpenStreetMap',
+			'UTFGrid',
+			'CORS',
+			'JSON',
+			'S3',
+			'AWS'
+		],
+		forbidden: [
+			'easy',
+			'easily',
+			'simply',
+			'simple',
+			'obviously',
+			'just',
+			'basically',
+			'procure',
+			'sexy',
+			'insane',
+			'clearly'
+		]
+	}
+};
